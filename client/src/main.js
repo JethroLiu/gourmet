79ca531f62c4b12edcef35dd2ecfc395eea27790
JethroLiu <JethroLiu@163.com>
2020-09-20 17:00:08 +0800

    前端axios下载

--- client/src/main.js
+++ client/src/main.js
@@ -1,10 +1,19 @@
-import Vue from 'vue'
-import App from './App.vue'
-import router from './router'
+import Vue from "vue";
+import App from "./App.vue";
+import router from "./router";
+import axios from "axios";
 
-Vue.config.productionTip = false
+// 配置全局的 axios 基础网址，可以只写路由了
+// 但是并不影响 绝对网络路径的请求
+axios.defaults.baseURL = "http://192.168.3.114:7001";
+
+// 允许请求时携带 cookie
+axios.defaults.withCredentials = true;
+Vue.prototype.$axios = axios;
+
+Vue.config.productionTip = false;
 
 new Vue({
-  router,
-  render: h => h(App)
-}).$mount('#app')
+    router,
+    render: (h) => h(App),
+}).$mount("#app");

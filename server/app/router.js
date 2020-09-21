047869ad238777d92068728040307a7be9429885
“cwm123456gzl <cwm123456gzl@163.com>
2020-09-21 19:09:19 +0800

    业务层代码

--- server/app/router.js
+++ server/app/router.js
@@ -5,5 +5,12 @@
  */
 module.exports = app => {
   const { router, controller } = app;
-  router.get('/', controller.home.index);
+  router.get('/lbt', controller.home.lbt);//lbt为轮播图请求地址
+  router.get('/advice', controller.home.advice);//advice 健康 烘焙 为您推荐
+  router.get("/food",controller.home.food)//book  具体菜品展示
+  router.get("/book",controller.home.book)//book  具体菜品展示
+  router.get("/topic",controller.home.topic)//book  具体菜品展示
+  
+
+
 };
--- server/config/config.default.js
+++ server/config/config.default.js
@@ -27,7 +27,7 @@ module.exports = (appInfo) => {
 
     // 跨域
     config.cors = {
-        origin: "http://192.168.3.29:8080",
+        origin: "http://127.0.0.1:8080",
         credentials: true,
     };
 
@@ -51,15 +51,15 @@ module.exports = (appInfo) => {
     };
 
     // 数据库配置
-    config.mysql = {
-        client: {
-            host: "localhost",
-            port: 3306,
-            user: "root",
-            password: "root",
-            database: "library",
-        },
-    };
+    // config.mysql = {
+    //     client: {
+    //         host: "localhost",
+    //         port: 3306,
+    //         user: "root",
+    //         password: "root",
+    //         database: "gourmet",
+    //     },
+    // };
 
     // add your user config here
     const userConfig = {

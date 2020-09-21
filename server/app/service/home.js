2631132866a9b820e52096ffcddb231fe05afb45
“cwm123456gzl <cwm123456gzl@163.com>
2020-09-21 20:29:06 +0800

    业务层

--- server/app/service/home.js
+++ server/app/service/home.js
@@ -5,35 +5,35 @@ class HomeService extends Service {
     
     //轮播图部分 
     async lbt() {
-        // var data = await this.app.mysql.query(" select * from lbt")
-        // return data;
-        return "轮播图  "
+        var data = await this.app.mysql.query(" select * from lbt")
+        return data;
+        // return "轮播图  "
     }
     //advice 健康 烘焙 为您推荐
     async advice() {
-        // var data = await this.app.mysql.query(" select * from advise")
-        // return data;
-        return "advice 健康 烘焙 为您推荐 "
+        var data = await this.app.mysql.query(" select * from advise")
+        return data;
+        // return "advice 健康 烘焙 为您推荐 "
     }
     //book  具体菜品展示
     async food() {
-        // var data = await this.app.mysql.query(" select * from food")
-        // return data;
-        return "book  具体菜品展示1 "
+        var data = await this.app.mysql.query(" select * from food")
+        return data;
+        // return "book  具体菜品展示1 "
     }
 
     //book2  具体菜品2展示
     async book() {
-        // var data = await this.app.mysql.query(" select * from book")
-        // return data;
-        return "book2  具体菜品展示2 "
+        var data = await this.app.mysql.query(" select * from book")
+        return data;
+        // return "book2  具体菜品展示2 "
     }
 
      //话题
      async topic() {
-        // var data = await this.app.mysql.query(" select * from topic")
-        // return data;
-        return "这是话题 "
+        var data = await this.app.mysql.query(" select * from topic")
+        return data;
+        // return "这是话题 "
     }
 
 

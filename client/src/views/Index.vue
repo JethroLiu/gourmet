f223e32bdac8391ce450f41698dcba49e7e9907a
“jxxxxx <jia@xxx.com>
2020-09-21 17:26:14 +0800

    轮播图组件修改

--- client/src/views/Index.vue
+++ client/src/views/Index.vue
@@ -1,23 +1,22 @@
 <template>
-  <div id="index">
-    <Topbar></Topbar>
-    <Logo></Logo>
-    <Lbt></Lbt>
-  </div>
+    <div id="index">
+        <Topbar></Topbar>
+        <Logo></Logo>
+        <Lbt></Lbt>
+    </div>
 </template>
 <script>
 import Topbar from "@/components/header/Topbar";
 import Logo from "@/components/header/Logo";
-import Lbt from "@/components/header/Lbt";
+import Lbt from "@/components/lbt/Lbt";
 
 export default {
-  components: {
-    Topbar,
-    Logo,
-    Lbt,
-  },
+    components: {
+        Topbar,
+        Logo,
+        Lbt,
+    },
 };
 </script>
 
-<style>
-</style>
+<style></style>

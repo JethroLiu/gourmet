1997c4167b12e189d35389dbec46b07936e2e8a8
lxlxlxlxlxllx <3118493414@qq.com>
2020-09-22 14:53:12 +0800

    话题

--- client/src/views/Index.vue
+++ client/src/views/Index.vue
@@ -4,6 +4,7 @@
     <Logo></Logo>
     <Lbt></Lbt>
     <Advice></Advice>
+    <topic></topic>
   </div>
 </template>
 
@@ -12,6 +13,7 @@ import Topbar from "@/components/header/Topbar";
 import Logo from "@/components/header/Logo";
 import Lbt from "@/components/lbt/Lbt";
 import Advice from "@/components/advice/Advice";
+import topic from "@/components/topic/content"
 
 export default {
   components: {
@@ -19,6 +21,7 @@ export default {
     Logo,
     Lbt,
     Advice,
+    topic,
   },
 };
 </script>

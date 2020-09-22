c6d4de89a299ca7b42d23fd4879e31a12919e8e5
rguangqin <rguangqin@163.com>
2020-09-22 14:44:55 +0800

    book

--- client/src/views/Index.vue
+++ client/src/views/Index.vue
@@ -4,6 +4,7 @@
     <Logo></Logo>
     <Lbt></Lbt>
     <Advice></Advice>
+    <Book></Book>
   </div>
 </template>
 
@@ -12,13 +13,14 @@ import Topbar from "@/components/header/Topbar";
 import Logo from "@/components/header/Logo";
 import Lbt from "@/components/lbt/Lbt";
 import Advice from "@/components/advice/Advice";
-
+import Book from "@/components/Book/Book";
 export default {
   components: {
     Topbar,
     Logo,
     Lbt,
     Advice,
+    Book,
   },
 };
 </script>

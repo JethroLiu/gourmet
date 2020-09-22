e3313931c910e4416d5d641ad3cad554bef83351
“jxxxxx <jia@xxx.com>
2020-09-22 15:05:56 +0800

    说明内容

---
+++ client/src/components/footer/Footer.vue
@@ -0,0 +1,42 @@
+<template>
+  <div id="footer" class="w">
+    <router-link to="##" class="redTitle">{{redText}}</router-link>
+    <p class="bout">
+      <router-link></router-link>
+    </p>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      redText: "美食天下 - 让吃更美好！",
+    };
+  },
+};
+</script>
+
+<style scoped>
+#footer {
+  position: relative;
+  top: 20px;
+  height: 166px;
+  border-top: 1px solid #e5e5e5;
+}
+
+.redTitle {
+  position: absolute;
+  top: 10px;
+  left: 0;
+  display: block;
+  font-size: 14px;
+  font-weight: bolder;
+  text-decoration: none;
+  color: #ff5500;
+}
+
+.redTitle:hover {
+  text-decoration: underline;
+}
+</style>
--- server/config/config.default.js
+++ server/config/config.default.js
@@ -26,7 +26,7 @@ module.exports = (appInfo) => {
     };
     // 跨域
     config.cors = {
-        origin: "http://127.0.0.1:8080",
+        origin: "http://192.168.3.114:8080",
         credentials: true,
     };
 

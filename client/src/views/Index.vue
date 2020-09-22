cd8ca9194c9cb9b519a92667dd03da50450e9643
lyl-09823 <2055208338@qq.com>
2020-09-22 15:00:35 +0800

    首页菜谱

--- client/src/views/Index.vue
+++ client/src/views/Index.vue
@@ -4,6 +4,8 @@
     <Logo></Logo>
     <Lbt></Lbt>
     <Advice></Advice>
+    <Title></Title>
+  
   </div>
 </template>
 
@@ -12,6 +14,11 @@ import Topbar from "@/components/header/Topbar";
 import Logo from "@/components/header/Logo";
 import Lbt from "@/components/lbt/Lbt";
 import Advice from "@/components/advice/Advice";
+import Title from "@/components/food/Title";
+
+
+
+
 
 export default {
   components: {
@@ -19,6 +26,8 @@ export default {
     Logo,
     Lbt,
     Advice,
+    Title,
+ 
   },
 };
 </script>

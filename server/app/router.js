b5bfd84b92b1ff2f95f38178e73c8478cdd88cef
“cwm123456gzl <cwm123456gzl@163.com>
2020-09-22 14:40:35 +0800

    后端首页所有cwm

--- server/app/router.js
+++ server/app/router.js
@@ -7,10 +7,7 @@ module.exports = app => {
   const { router, controller } = app;
   router.get('/lbt', controller.home.lbt);//lbt为轮播图请求地址
   router.get('/advice', controller.home.advice);//advice 健康 烘焙 为您推荐
-  router.get("/food",controller.home.food)//book  具体菜品展示
-  router.get("/book",controller.home.book)//book  具体菜品展示
-  router.get("/topic",controller.home.topic)//book  具体菜品展示
-  
-
-
+  router.get("/food",controller.home.food)//food  新秀菜谱 一周热度 
+  router.get("/book",controller.home.book)//book  时令食材
+  router.get("/topic",controller.home.topic)//话题
 };
